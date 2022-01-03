a64bbece8be45862e673566de792dfd253d3f47c
Vit0orS <vitordasilveira01@gmail.com>
2022-01-03 15:46:04 -0300

    Mudanças pesquisa CEP, checa APIs antes, + mensagem de erro

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,24 +55,35 @@ function gatoPorCEP(){
     const dataPerdidoTexto = document.querySelectorAll(".dataPerdido");
     const localTexto = document.querySelectorAll(".localidade");
     
+    for (let i=0; i<3; i++){
+        imagemSrc[i].src = "../assets/img/loading-buffering.gif"
+    }
+    const timeout = setTimeout(() => {
+        for (let i=0; i<3; i++){
+            imagemSrc[i].src = "../assets/img/errorImage.png";
+            nomeTexto[i].innerHTML = `Não foi possível encontrar gatos neste endereço`;
+            encontradoTexto[i].innerHTML = `Talvez você digitou errado, ou o servidor está fora...`;
+            dataPerdidoTexto[i].innerHTML = '';
+            localTexto[i].innerHTML = '';
+        }
+    }, 6000)
+
     dadosCEP(CEP)
     .then(local => {
-        for (let i=0; i<3; i++){
-            imagemSrc[i].src = "../assets/img/loading-buffering.gif"
-            const data = new generateRandomDate().toLocaleDateString('pt-BR');
-            const encontrado = Math.floor(Math.random() * (10 - 1)) + 1;
-            const nomeDoGato = nomes[Math.floor(Math.random() * nomes.length)];
-            nomeTexto[i].innerHTML = `Nome: ${nomeDoGato}`;
-            encontradoTexto[i].innerHTML = `Encontrado? ${encontrado%2==0 ? "SIM" : "NÃO"}`;
-            dataPerdidoTexto[i].innerHTML = `Data em que sumiu: ${data}`;
-            localTexto[i].innerHTML = `Perto do bairro: ${local.bairro}`;
-        }
     
         for (let i=0; i<3; i++){
             catApiGet()
             .then(dado => {
+                clearTimeout(timeout);
                 console.log(dado);
                 document.querySelectorAll(".imagemGato")[i].src = dado.webpurl;
+                const data = new generateRandomDate().toLocaleDateString('pt-BR');
+                const encontrado = Math.floor(Math.random() * (10 - 1)) + 1;
+                const nomeDoGato = nomes[Math.floor(Math.random() * nomes.length)];
+                nomeTexto[i].innerHTML = `Nome: ${nomeDoGato}`;
+                encontradoTexto[i].innerHTML = `Encontrado? ${encontrado%2==0 ? "SIM" : "NÃO"}`;
+                dataPerdidoTexto[i].innerHTML = `Data em que sumiu: ${data}`;
+                localTexto[i].innerHTML = `Perto do bairro: ${local.bairro}`
             })
         }
     })
